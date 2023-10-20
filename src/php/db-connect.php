8519eaeef6bfaf52ccdab2302d881e65b878ed06
koshi <2201035@s.asojuku.ac.jp>
2023-10-20 12:05:18 +0900

    接続プログラムを追加

---
+++ src/php/db-connect.php
@@ -0,0 +1,9 @@
+<?php
+//自分のものを入力
+    const SERVER = '';
+    const DBNAME = '';
+    const USER = '';
+    const PASS = '';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
