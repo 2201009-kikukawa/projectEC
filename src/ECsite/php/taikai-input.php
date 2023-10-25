8b830d036c2949b481e87d0c47652714c2191cc1
momo <momomo.momoko.0665@icloud.com>
2023-10-25 15:13:47 +0900

    退会ページつくりました　完了画面は使いまわしできると思います

---
+++ src/ECsite/php/taikai-input.php
@@ -0,0 +1,17 @@
+<?php require 'menu.php'; ?>
+<link rel="stylesheet" href="../../css/taikai.css">
+<h2>退会ページ</h2>
+<h1>パスワード確認</h1>
+<p>アカウント削除のリクエストを完了するにはアカウントに登録された</br>パスワードを入力してください</p>
+
+
+
+
+<form action="taikai-output.php" method="post">
+        <div class="contents-container">
+           
+           
+            <input type="password" name="password" class="form-input" required /><br>
+            <button type="submit" class="button-input">アカウント削除</button>
+        </div>
+    </form>
