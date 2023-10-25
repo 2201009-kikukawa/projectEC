a4e0f49d54b4c962df4e09b0e4d28638d0e3328d
Gingira1213 <gin12132828@icloud.com>
2023-10-25 14:08:59 +0900

    クレカ有効期限修正

--- src/ECsite/script/member-register.js
+++ src/ECsite/script/member-register.js
@@ -7,9 +7,9 @@ for (let i = 1; i <= 12; i++) {
     monthSelect.appendChild(option);
 }
 
-// 日のオプションを生成
+// 年のオプションを生成
 const daySelect = document.getElementById('day');
-for (let i = 1; i <= 31; i++) {
+for (let i = 2023; i <= 2040; i++) {
     const option = document.createElement('option');
     option.value = i;
     option.text = i ;
