cb4ae8915c43f051ac200ca21a46f2fe95074bb4
Gingira1213 <gin12132828@icloud.com>
2023-11-07 10:53:14 +0900

    会員登録完了画面のDBに対する処理

--- src/ECsite/php/member-register-completed.php
+++ src/ECsite/php/member-register-completed.php
@@ -1,3 +1,40 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
+<?php
+$password_hash = password_hash($_POST['register_pass'], PASSWORD_DEFAULT);
+
+$pdo = new PDO($connect, USER, PASS);
+
+$register_birthdate = $_POST['register_birthdate']; // フォームからの日付の値
+
+// 日付のフォーマットを変更
+$birthdate_formatted = date('Y-m-d', strtotime($register_birthdate));
+
+$sql = $pdo->prepare('insert into menber values(null,?,?,?,?,?,?,?,null)');
+$sql->execute([
+    $_POST['register_mell'], $password_hash,
+    $_POST['register_account_name'],$birthdate_formatted,
+    $_POST['gender'], $_POST['register-postcode'],
+    $_POST['register_address']
+]);
+
+
+$month = $_POST['month']; // フォームから選択された月の値
+$year = $_POST['year'];   // フォームから選択された年の値
+
+// 有効期限を年月の形式で文字列として結合
+$expiration_date = "{$year}-{$month}-28"; // 仮に日付を1日として設定
+
+// 文字列をDATE型に変換
+$expiration_date = date('Y-m-d', strtotime($expiration_date));
+
+$sql = $pdo->prepare('insert into credit values(null,null,?,?,?,null)');
+$sql->execute([
+    $_POST['credit_name'],$expiration_date,
+    $_POST['credit-input']
+]);
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
