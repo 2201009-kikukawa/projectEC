5816e13b5c5858b697d8177b2fa3c141ae8d40a9
Gingira1213 <gin12132828@icloud.com>
2023-11-08 14:58:20 +0900

    スペース

--- src/ECsite/php/member-register-completed.php
+++ src/ECsite/php/member-register-completed.php
@@ -19,6 +19,8 @@ $sql->execute([
 ]);
 
 
+
+
 $month = $_POST['month']; // フォームから選択された月の値
 $year = $_POST['year'];   // フォームから選択された年の値
 
@@ -28,7 +30,7 @@ $expiration_date = "{$year}-{$month}-28"; // 仮に日付を1日として設定
 // 文字列をDATE型に変換
 $expiration_date = date('Y-m-d', strtotime($expiration_date));
 
-$sql = $pdo->prepare('insert into credit values(null,null,?,?,?)');
+$sql = $pdo->prepare('insert into credit values(null,,?,?,?)');
 $sql->execute([
     $_POST['credit_name'],$expiration_date,
     $_POST['securitycord']
