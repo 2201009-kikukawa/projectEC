9641ccfe959c6ea192be65f6deb4042bee5b911b
Gingira1213 <gin12132828@icloud.com>
2023-11-08 13:48:29 +0900

    クレカ有効期限修正

--- src/ECsite/php/member-register.php
+++ src/ECsite/php/member-register.php
@@ -60,8 +60,8 @@
             <span class="error" id="credit-card-number-error" style="color: red;"></span>
             <br><br>
             クレジットカード有効期間
-            <select id="month" class="credit-input"></select>月
-            <select id="day" class="credit-input"></select>年<br /><br>
+            <select id="month" name="month"class="credit-input"></select>月
+            <select id="day" name="year" class="credit-input"></select>年<br /><br>
             セキュリティコード
             <input type="text" name="securitycord" class="credit-input" id="security-code" />
             <span class="error" id="security-code-error" style="color: red;"></span>
