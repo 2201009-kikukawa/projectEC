4ad12ebc6557fb33db497ea1b933155a1f6d0890
Gingira1213 <gin12132828@icloud.com>
2023-11-09 12:22:08 +0900

    会員登録完了画面　DB関係

--- src/ECsite/php/member-register-completed.php
+++ src/ECsite/php/member-register-completed.php
@@ -18,7 +18,8 @@ $sql->execute([
     $_POST['register_address'],$_POST['credit']
 ]);
 
-
+// 会員IDを取得
+$member_id = $pdo->lastInsertId();
 
 
 $month = $_POST['month']; // フォームから選択された月の値
@@ -30,9 +31,9 @@ $expiration_date = "{$year}-{$month}-28"; // 仮に日付を1日として設定
 // 文字列をDATE型に変換
 $expiration_date = date('Y-m-d', strtotime($expiration_date));
 
-$sql = $pdo->prepare('insert into credit values(null,,?,?,?)');
+$sql = $pdo->prepare('insert into credit values(null,?,?,?,?)');
 $sql->execute([
-    $_POST['credit_name'],$expiration_date,
+    $member_id,$_POST['credit_name'],$expiration_date,
     $_POST['securitycord']
 ]);
 ?>
