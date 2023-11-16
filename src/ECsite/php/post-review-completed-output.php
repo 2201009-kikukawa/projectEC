9c530c30ce8996b7c7b22d24dc64aa704807ac2a
Gingira1213 <gin12132828@icloud.com>
2023-11-16 09:13:48 +0900

    post-review-completed-output.php

---
+++ src/ECsite/php/post-review-completed-output.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
+<?php
+$product_id = $menber_id = '';
+$product_id = $_SESSION['product']['id'];
+$menber_id = $_SESSION['menber']['id'];
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('insert into review values(null,?,?,?,?,NOW())');
+$sql->execute([
+    $product_id, $menber_id,
+    $_POST['raring'],$_POST['review']
+]);
+?>
