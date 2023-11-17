634202675e043c8b9b678b8bd1d41ccdae3e289e
Gingira1213 <gin12132828@icloud.com>
2023-11-17 09:21:46 +0900

    新規商品登録画面  css

--- src/adminsite/php/product-register.php
+++ src/adminsite/php/product-register.php
@@ -18,36 +18,57 @@
             <div class="required_item">
                 <h3 class="required">必須項目</h3>
             </div>
-            <h3 class="required">商品名</h3>
-            <input type="text" name="register_mell" class="form-input" id="email" required />
-            <h3 class="required">魚の種類</h3>
-            <select>
-                <option value="1">魚種別</option>
-                <option value="2">貝類・魚卵</option>
-                <option value="3">加工品</option>
-            </select>
+            <div class="container">
+                <div class="container-left">
+                    <h3 class="required">商品名</h3>
+                    <input type="text" name="product_name" class="form-input" required />
+                    <br>
+                    <h3 class="required">魚の種類</h3>
+                    <select>
+                        <option value="1">魚種別</option>
+                        <option value="2">貝類・魚卵</option>
+                        <option value="3">加工品</option>
+                    </select>
+                    <h5>更に詳しく</h5>
+                    <select>
+                        <option value="1">マグロ</option>
+                        <option value="2">サーモン</option>
+                        <option value="3">イカ</option>
+                        <option value="4">イクラ</option>
+                        <option value="5">うに</option>
+                        <option value="6">とびっこ</option>
+                        <option value="7">魚の缶詰</option>
+                        <option value="8">干しもの</option>
+                        <option value="9">調理済み食品</option>
+                    </select>
+                    <br>
+                    <h3 class="required">出品先選択</h3>
+                    <select>
+                        <option value="1">オークション商品</option>
+                        <option value="2">一般商品</option>
+                    </select>
+                </div>
 
-            <select>
-                <option value="1">マグロ</option>
-                <option value="2">サーモン</option>
-                <option value="3">イカ</option>
-                <option value="4">イクラ</option>
-                <option value="5">うに</option>
-                <option value="6">とびっこ</option>
-                <option value="7">魚の缶詰</option>
-                <option value="8">干しもの</option>
-                <option value="9">調理済み食品</option>
-            </select>
-
-            <h3 class="required">出品先選択</h3>
-            <select>
-                <option value="1">オークション商品</option>
-                <option value="2">一般商品</option>
-            </select>
+                <div class="container-right">
+                    <h3 class="required">商品の詳細記入欄</h3>
+                    <textarea name="explanation" rows="6" cols="60"></textarea>
+                    <br>
+                    <h3 class="required">価格</h3>
+                    <input type="text" name="price" class="form-input-price" required />
+                    <br>
+                    <h3 class="required">個数</h3>
+                    <input type="text" name="number" class="form-input-number" required />
+                </div>
+            </div>
+            <br>
+            <div class="image">
+                <h3 class="required">画像の添付</h3>
+                <input type="file" id="image" name="image" class="" accept="image/*">
+            </div>
 
             <button type="submit" class="button-input">登録</button>
             <!-- jsの読み込み -->
-            <script src="../script/member-register.js"></script>
+            <script src="../script/product-register.js"></script>
             <script src="../script/validate/product-register-validate.js"></script>
         </div>
     </form>
