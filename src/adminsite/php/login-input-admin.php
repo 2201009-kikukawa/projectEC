07868bfe5d1f88977cc75e50c2bff89655936316
Gingira1213 <gin12132828@icloud.com>
2023-11-22 13:55:23 +0900

    管理者ログイン

---
+++ src/adminsite/php/login-input-admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Login Form</title>
+  <link rel="stylesheet" href="../css/login-.css">
+</head>
+
+<body>
+  <?php include("./menu.php"); ?>
+  <form action="login-output-admin.php" method="POST">
+    <h1>鮮魚Online</h1>
+    <h3>管理者ログインページ</h3>
+    <div class="form-input">
+      <div class="form-input-account">
+        <label for="name">アカウント名:</label>
+        <input type="text" class="account_name" id="name" name="name" required><br>
+      </div>
+      <div class="form-input-password">
+        <label for="pass">パスワード:</label>
+        <input type="password" class="password" id="pass" name="pass" required>
+      </div>
+      <button type="submit" class="button-input">ログイン</button>
+    </div>
+    <div class="center">
+      <a href="member-register.php">
+        新規登録はこちらから
+      </a>
+    </div>
+    <img src="../image/umi2.png" alt="umi2" class="img">
+  </form>
+</body>
+
+</html>
---
+++ src/adminsite/php/login-output-admin.php
@@ -0,0 +1,44 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Form</title>
+    <link rel="stylesheet" href="../css/login-output-admin.css">
+</head>
+
+<body>
+    <?php include("./menu.php"); ?>
+</body>
+
+</html>
+<?php
+unset($_SESSION['member']);
+$pdo = new PDO($connect, USER, PASS);
+
+$sql = $pdo->prepare('select * from member where account_name=:name');
+$sql -> bindParam(':name',$_POST['name']);
+$sql->execute();
+$row = $sql->fetch();
+$password_hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+if ($row && password_verify($_POST['pass'], $row['PASSWORD'])) {
+    $_SESSION['member'] = [
+        'id' => $row['member_id'],
+        'mell' => $row['mell'],
+        'password' => $row['PASSWORD'],
+        'account_name' => $row['account_name'],
+        'birthday' => $row['birthday'],
+        'gender' => $row['gender'],
+        'post_num' => $row['post_num'],
+        'address' => $row['address'],
+        'payment_id' => $row['payment_id']
+    ];
+    // ログイン成功時の処理
+    echo '<script>window.location.href = "top.php";</script>';
+    exit(); // リダイレクトしたらスクリプトの実行を終了
+} else {
+    echo 'ログイン名またはパスワードが違います。';
+}
+?>
