dc8bc54e8021716c45d509af85ff45b2278a7cac
Gingira1213 <gin12132828@icloud.com>
2023-11-22 16:55:03 +0900

    新規商品登録完了　form　とび先

--- src/adminsite/php/product-register-completed.php
+++ src/adminsite/php/product-register-completed.php
@@ -1,10 +1,9 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
-<?php include("./menu.php"); ?>
 <?php
 $pdo = new PDO($connect, USER, PASS);
 
-$sql = $pdo->prepare('insert into product values(null,?,?,?,?,null,?,?,?,?,?)');
+$sql = $pdo->prepare('insert into product values(null,?,?,?,?,?,CURRENT_TIMESTAMP,?,?,?,?)');
 $sql->execute([
   $_POST['product_name'], $_POST['explanation'],
   $_POST['price'], 0,
@@ -23,9 +22,10 @@ $sql->execute([
 </head>
 
 <body>
+<?php include("./menu.php"); ?>
   <h3>登録が完了しました</h3>
-  <form action="admin-top" method="POST"> <!-- フォームのアクションを適切なファイルに指定する -->
-    <button type="submit" class="button-input">画面TOPへ戻る</button>
+  <form action="product-register.php" method="POST"> <!-- フォームのアクションを適切なファイルに指定する -->
+    <button type="submit" class="button-input">新規商品登録へ戻る</button>
   </form>
   </div>
 </body>
