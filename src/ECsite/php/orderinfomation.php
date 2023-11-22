c7b54e25df4c5f33d303a6ad17f6fe0a70f32f68
8L1R8 <137019656+8L1R8@users.noreply.github.com>
2023-11-22 14:03:51 +0900

    11/22_4

--- src/ECsite/php/orderinfomation.php
+++ src/ECsite/php/orderinfomation.php
@@ -25,8 +25,8 @@
         <input type="text" id="hi" name="hi" placeholder="年/月/日" required><br><br>
 
         <div class="button-container">
-            <button type="submit" class="button-input">戻る</button>
-            <button type="submit" class="button-input">確定</button>
+            <button type="submit" class="button-post">戻る</button>
+            <button type="submit" class="button-back">確定</button>
         </div>
     </center>
     </form>
