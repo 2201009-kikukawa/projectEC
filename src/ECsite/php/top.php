221cc3894ca8a10bba9637bb49de87f30927c6b0
momo <momomo.momoko.0665@icloud.com>
2023-11-27 13:55:01 +0900

    トップ画面　スライドショーと　キーワード検索のボックスを表示させました　検索ボックスだけ配置してます

--- src/ECsite/php/top.php
+++ src/ECsite/php/top.php
@@ -1,6 +1,46 @@
 <?php require 'menu.php'; ?>
 <link rel="stylesheet" href="../css/top.css">
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Simple Slideshow</title>
+<link rel="stylesheet" href="../css/top.css"> 
+</head>
+<body>
+<!-- アニメーション（umianime.php） -->
 <?php require 'umianime.php'; ?>
+
+
+<div class="centered-search">
+  <form method="post" action="searchproductshow.php" class="search_container">
+    <input type="text" size="10" name="key" placeholder="キーワード検索">
+    <input type="submit" value="&#xf002">
+  </form>
+</div>
+
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <img src="../image/umi1.png" style="width:300px">
+    </div>
+
+    <div class="mySlides fade">
+        <img src="../image/肉.png" style="width:300px">
+    </div>
+
+    <div class="mySlides fade">
+        <img src="../image/my.png" style="width:300px">
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+
+
+<script src="../script/top.js"></script>
+
+</body>
+</html>
 
---
+++ src/ECsite/script/top.js
@@ -0,0 +1,23 @@
+ 
+var slideIndex = 0;
+showSlidesAuto();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+function showSlidesAuto() {
+  showSlides(slideIndex += 1);
+  setTimeout(showSlidesAuto, 10000); 
+}
