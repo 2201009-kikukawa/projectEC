748ffb0305d9e54573f984901ddd3dadf1b68ddd
kikukawa <2201009@s.asojuku.ac.jp>
2023-11-30 11:56:44 +0900

    カミングスーンページ作成。

---
+++ src/ECsite/php/ComingSoon.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ComingSoon</title>
+    <link rel="stylesheet" href="../css/ComingSoon.css">
+</head>
+<body>
+    <?php require 'menu.php'?>
+    <?php require 'umianime.php'; ?>
+    <div class = "comingarea">
+        <div class ="comingsoon">ComingSoon</div>
+        <div class ="comingsoon2">このページは近日公開予定です。</div>
+        <div class ="link"><a href="top.php" class ="alink">トップページに戻る</a></div>
+    </div>
+</body>
+</html>
--- src/ECsite/php/mypage.php
+++ src/ECsite/php/mypage.php
@@ -4,7 +4,7 @@
 
 <div class="my-buttons">
     <button onclick="window.location.href='./top.php'" class="top-button">トップへ</button>
-    <button onclick="window.location.href='#'" class="my-button">注文履歴</button>
+    <button onclick="window.location.href='ComingSoon.php'" class="my-button">注文履歴</button>
     <button onclick="window.location.href='./member-update-check-input.php'" class="my-button">会員情報更新</button>
     <button onclick="window.location.href='./taikai-input.php'" class="my-button">退会</button>
 </div>
