581fba32357292d7cab352a1845c3edd7b5d9009
Gingira1213 <gin12132828@icloud.com>
2023-12-01 18:25:38 +0900

    オークション終了画面

--- src/ECsite/php/auctionfinish.php
+++ src/ECsite/php/auctionfinish.php
@@ -5,15 +5,30 @@ if (session_status() == PHP_SESSION_NONE) {
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>オークション終了画面</title>
     <link rel="stylesheet" href="../css/auctionfinish.css">
 </head>
-<body>
-    <?php require 'get-auctiondetail.php' ?>
-   
 
-    
+<body>
+    <!-- ヘッダーの読み込み -->
+    <?php include("./menu.php"); ?>
+    <?php require 'get-auctiondetail.php' ?>
+    <?php
+    echo '<div class="contents-container">';
+    echo '<div class="contents-container-sub">';
+    echo '<img alt="image" class="image" src="../image/', $product['picture'], '">';
+    echo '<div class="product-info">';
+    echo "<p class='item'>{$product['product_name']}</p>";
+    echo "<p class='item'>{$product['currentprice']} 円</p>";
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    ?>
+
+
 </body>
+
 </html>
