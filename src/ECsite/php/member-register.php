f5bf5d9626db5891b113abc5be8f4d7f16c4a5a1
Gingira1213 <gin12132828@icloud.com>
2023-12-06 12:41:53 +0900

    行間隔

--- src/ECsite/php/member-register.php
+++ src/ECsite/php/member-register.php
@@ -41,42 +41,34 @@ if (session_status() == PHP_SESSION_NONE) {
             <h2 class="required">メールアドレス</h2>
             <input type="text" name="register_mell" class="form-input" id="email" required />
             <span class="error" id="email-error" style="color: red;"></span>
-            <br>
             <h2 class="required">パスワード</h2>
             <div class="pass-eye">
                 <input type="password" name="register_pass" class="form-input" id="password" required />
                 <span id="buttonEye" class="fa fa-eye" onclick="pushHideButton()"></span>
             </div>
             <span class="error" id="password-error" style="color: red;"></span>
-            <br>
             <h2 class="required">アカウント名</h2>
             <input type="text" name="register_account_name" class="form-input" id="account-name" required />
             <span class="error" id="account-name-error" style="color: red;"></span>
-            <br>
             <h2 class="required">生年月日</h2>
             <input type="text" name="register_birthdate" class="form-input" id="birthdate" required />
             <span class="error" id="birthdate-error" style="color: red;"></span>
-            <br>
             <h2 class="required">性別</h2>
             <input type="radio" name="gender" value="1" />女性<br />
             <input type="radio" name="gender" value="2" />男性<br />
             <input type="radio" name="gender" value="3" />その他<br />
             <span class="error" id="gender-error" style="color: red;"></span>
-            <br>
             <h2 class="required">郵便番号</h2>
             <input type="text" name="register-postcode" class="form-input" id="postcode" required />
             <span class="error" id="postcode-error" style="color: red;"></span>
-            <br>
             <h2 class="required">住所</h2>
             <input type="text" name="register_address" class="form-input" id="address" required />
             <span class="error" id="address-error" style="color: red;"></span>
-            <br>
             <h3 class="required">お支払方法の登録</h3>
             <input type="radio" name="credit" value="1" class="custom-radio" onclick="toggleCreditCardFields(true)" />クレジットカード・デビットカード<br />
             <input type="radio" name="credit" value="2" class="custom-radio" onclick="toggleCreditCardFields(false)" />コンビニ払い<br />
             <input type="radio" name="credit" value="3" class="custom-radio" onclick="toggleCreditCardFields(false)" />携帯払い<br />
             <span class="error" id="payment-error" style="color: red;"></span>
-            <br>
             <h2>クレジットを登録されたお客様</h2>
             クレジットカード・デビットカード
             <input type="text" name="credit_name" class="credit_debit-input" id="credit-card-number" required />
