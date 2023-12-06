6889e229cc95ef653af64c0707d7eb6ae8e6dbb9
kikukawa <2201009@s.asojuku.ac.jp>
2023-12-06 13:50:09 +0900

    退会前にログインチェックと確認を追加

--- src/ECsite/php/taikai-input.php
+++ src/ECsite/php/taikai-input.php
@@ -10,14 +10,22 @@ if (session_status() == PHP_SESSION_NONE) {
 <h1>パスワード確認</h1>
 <p>アカウント削除のリクエストを完了するにはアカウントに登録された</br>パスワードを入力してください</p>
 
+<form action="taikai-output.php" method="post" onsubmit="return confirmAccountDeletion()">
+    <div class="contents-container">
+        <input type="password" name="password" class="form-input" required /><br>
+        <button type="submit" class="button-input">アカウント削除</button>
+    </div>
+</form>
 
-
-
-<form action="taikai-output.php" method="post">
-        <div class="contents-container">
-           
-           
-            <input type="password" name="password" class="form-input" required /><br>
-            <button type="submit" class="button-input">アカウント削除</button>
-        </div>
-    </form>
+<script>
+function confirmAccountDeletion() {
+    <?php
+    if (isset($_SESSION['member'])) {
+        echo 'return confirm("アカウントを削除しますか？");';
+    } else {
+        echo 'alert("アカウントを削除するにはログインしてください");';
+        echo 'return false;'; // PHPから直接 false を返すように修正
+    }
+    ?>
+}
+</script>
